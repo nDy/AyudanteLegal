e3e489277ae7704510cc9f31beeb70a1f302bfce
Andres Rangel <nDy.Work@gmail.com>
2013-07-08 22:10:21 -0430

    zoom funcional

--- AyudanteLegal/mainwindow.cpp
+++ AyudanteLegal/mainwindow.cpp
@@ -2,82 +2,26 @@
 #include "ui_mainwindow.h"
 #include "dirent.h"
 #include "stdio.h"
-#include "QLabel"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-//Menu
-    QMenu* Archivo;
-        QMenu* ArchivoNuevo;
-            QList<QAction*> ArchivoNuevoLista;
-        QAction* ArchivoAbrir;
-        QAction* ArchivoImprimir;
-        QAction* ArchivoGuardar;
-        QAction* ArchivoGuardarComo;
 
-    QMenu* Ver;
-        QAction* VerZoom;
-        QAction* VerHoja;
-        QAction* VerPagina;
-        QAction* VerEdicion;
+    scaleFactor = 1.0;
 
-    QMenu* Edicion;
-        QAction* EdicionCopiar;
-        QAction* EdicionCortar;
-        QAction* EdicionPegar;
+    this->GenerarMenu();
 
-    QMenu* Formato;
-        QMenu* FormatoFuente;
-            QList<QAction*> FormatoFuenteLista;
-        QAction* FormatoBase;
-        QAction* FormatoMinimo;
+    this->GenerarConexiones();
+}
 
-//Menu Archivo
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
 
-
-    Archivo = new QMenu;
-    Archivo->setTitle("Archivo");
-
-    ArchivoNuevo = new QMenu;
-    ArchivoNuevo->setTitle("Nuevo");
-
-    //Carga de planillas
-    DIR *dir;
-    struct dirent *ent;
-    if ((dir = opendir ("../AyudanteLegal/Planillas")) != NULL) {
-      //Lee Los nombres de las formas
-      while ((ent = readdir (dir)) != NULL) {
-          printf ("Carpeta: %s\n", ent->d_name);
-          DIR *formadir;
-          struct dirent *formaent;
-          if ((formadir = opendir (ent->d_name)) != NULL) {
-            //Lee componentes de la forma
-            while ((formaent = readdir (dir)) != NULL) {
-                 if(formaent->d_name[0]!='.'){
-                     printf ("      Carpeta %s\n", formaent->d_name);
-                     QAction* aux;
-                     static QString* file;
-                     file = new QString();
-                     file->append("../AyudanteLegal/Planillas/");
-                     file->append(formaent->d_name);
-                     file->append("/");
-                     aux = new QAction(ArchivoNuevo);
-                     aux->setText(formaent->d_name);
-                     printf ("%s\n", file->toStdString().c_str());
-                     aux->setObjectName(*file);
-                     ArchivoNuevo->addAction(aux);
-                 }
-            }
-          }
-      }
-      closedir (dir);
-    } else {
-      /* could not open directory */
-      perror ("");
-    }
+void MainWindow::GenerarConexiones(){
 
     QList<QAction*>::iterator j;
     for (j = ArchivoNuevo->actions().begin(); j != ArchivoNuevo->actions().end(); ++j) {
@@ -85,58 +29,108 @@ MainWindow::MainWindow(QWidget *parent) :
                          this, SLOT(CargaPlanilla()));
     }
 
-    Archivo->addMenu(ArchivoNuevo);
 
-    ArchivoAbrir = new QAction(Archivo);
-    ArchivoAbrir->setText("Abrir");
-    Archivo->addAction(ArchivoAbrir);
-
-    ArchivoGuardar = new QAction(Archivo);
-    ArchivoGuardar->setText("Guardar");
-    Archivo->addAction(ArchivoGuardar);
-
-    ArchivoGuardarComo = new QAction(Archivo);
-    ArchivoGuardarComo->setText("Guardar Como");
-    Archivo->addAction(ArchivoGuardarComo);
-
-//Menu Ver
-    Ver = new QMenu;
-    Ver->setTitle("Ver");
-
-    VerEdicion = new QAction(Ver);
-    VerEdicion->setText("Vista de Edicion");
-    Ver->addAction(VerEdicion);
-
-    VerHoja = new QAction(Ver);
-    VerHoja->setText("Vista de Hoja");
-    Ver->addAction(VerHoja);
-
-    VerPagina = new QAction(Ver);
-    VerPagina->setText("Vista de Pagina");
-    Ver->addAction(VerPagina);
-
-    VerZoom = new QAction(Ver);
-    VerZoom->setText("Vista de Edicion");
-    Ver->addAction(VerZoom);
-
-//Menu Edicion
-    Edicion = new QMenu;
-    Edicion->setTitle("Edicion");
-
-//Menu Formato
-    Formato = new QMenu;
-    Formato->setTitle("Formato");
-
-    ui->menuBar->addMenu(Archivo);
-    ui->menuBar->addMenu(Ver);
-    ui->menuBar->addMenu(Edicion);
-    ui->menuBar->addMenu(Formato);
+    connect(VerZoomIn, SIGNAL(triggered()), this, SLOT(ZoomIn()));
+    connect(VerZoomOut, SIGNAL(triggered()), this, SLOT(ZoomOut()));
 
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+void MainWindow::GenerarMenu(){
+    //Menu Archivo
+
+
+        Archivo = new QMenu;
+        Archivo->setTitle("Archivo");
+
+        ArchivoNuevo = new QMenu;
+        ArchivoNuevo->setTitle("Nuevo");
+
+        //Carga de planillas
+        DIR *dir;
+        struct dirent *ent;
+        if ((dir = opendir ("../AyudanteLegal/Planillas")) != NULL) {
+          //Lee Los nombres de las formas
+          while ((ent = readdir (dir)) != NULL) {
+              printf ("Carpeta: %s\n", ent->d_name);
+              DIR *formadir;
+              struct dirent *formaent;
+              if ((formadir = opendir (ent->d_name)) != NULL) {
+                //Lee componentes de la forma
+                while ((formaent = readdir (dir)) != NULL) {
+                     if(formaent->d_name[0]!='.'){
+                         printf ("      Carpeta %s\n", formaent->d_name);
+                         QAction* aux;
+                         static QString* file;
+                         file = new QString();
+                         file->append("../AyudanteLegal/Planillas/");
+                         file->append(formaent->d_name);
+                         file->append("/");
+                         aux = new QAction(ArchivoNuevo);
+                         aux->setText(formaent->d_name);
+                         printf ("%s\n", file->toStdString().c_str());
+                         aux->setObjectName(*file);
+                         ArchivoNuevo->addAction(aux);
+                     }
+                }
+              }
+          }
+          closedir (dir);
+        } else {
+          /* could not open directory */
+          perror ("");
+        }
+
+        Archivo->addMenu(ArchivoNuevo);
+
+        ArchivoAbrir = new QAction(Archivo);
+        ArchivoAbrir->setText("Abrir");
+        Archivo->addAction(ArchivoAbrir);
+
+        ArchivoGuardar = new QAction(Archivo);
+        ArchivoGuardar->setText("Guardar");
+        Archivo->addAction(ArchivoGuardar);
+
+        ArchivoGuardarComo = new QAction(Archivo);
+        ArchivoGuardarComo->setText("Guardar Como");
+        Archivo->addAction(ArchivoGuardarComo);
+
+    //Menu Ver
+        Ver = new QMenu;
+        Ver->setTitle("Ver");
+
+        VerEdicion = new QAction(Ver);
+        VerEdicion->setText("Vista de Edicion");
+        Ver->addAction(VerEdicion);
+
+        VerHoja = new QAction(Ver);
+        VerHoja->setText("Vista de Hoja");
+        Ver->addAction(VerHoja);
+
+        VerPagina = new QAction(Ver);
+        VerPagina->setText("Vista de Pagina");
+        Ver->addAction(VerPagina);
+
+        VerZoomIn = new QAction(Ver);
+        VerZoomIn->setText("ZoomIn");
+        Ver->addAction(VerZoomIn);
+
+        VerZoomOut = new QAction(Ver);
+        VerZoomOut->setText("ZoomOut");
+        Ver->addAction(VerZoomOut);
+
+    //Menu Edicion
+        Edicion = new QMenu;
+        Edicion->setTitle("Edicion");
+
+    //Menu Formato
+        Formato = new QMenu;
+        Formato->setTitle("Formato");
+
+        ui->menuBar->addMenu(Archivo);
+        ui->menuBar->addMenu(Ver);
+        ui->menuBar->addMenu(Edicion);
+        ui->menuBar->addMenu(Formato);
+
 }
 
 void MainWindow::CargaPlanilla()
@@ -150,13 +144,35 @@ void MainWindow::CargaPlanilla()
         aux.append(ruta);
         aux.append("Imagen.png");
         printf ("string auxiliar: %s\n", aux.toStdString().c_str());
-        QLabel* label;
-        label = new QLabel(ui->centralWidget);
-        QPixmap* pic;
+        label = new QLabel;
         pic = new QPixmap(aux);
-        QLayout *vbl = ui->centralWidget->layout();
-        vbl->addWidget(label);
         label->setPixmap(*pic);
+        ui->scrollArea->setWidget(label);
+        scaleImage(0.343289086);
      }
 
+}
+
+void MainWindow::ZoomIn()
+{
+        scaleImage(1.25);
+}
+void MainWindow::ZoomOut()
+{
+        scaleImage(0.75);
+}
+
+void MainWindow::scaleImage(double factor){
+    scaleFactor *= factor;
+    label->setPixmap(pic->scaled(pic->width()*scaleFactor,pic->height()*scaleFactor));
+
+    adjustScrollBar(ui->scrollArea->horizontalScrollBar(), factor);
+    adjustScrollBar(ui->scrollArea->verticalScrollBar(), factor);
+
+}
+
+void MainWindow::adjustScrollBar(QScrollBar *scrollBar, double factor)
+{
+    scrollBar->setValue(int(factor * scrollBar->value()
+                            + ((factor - 1) * scrollBar->pageStep()/2)));
 }
--- AyudanteLegal/mainwindow.h
+++ AyudanteLegal/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QScrollBar>
 
 namespace Ui {
 class MainWindow;
@@ -16,11 +18,46 @@ public:
 
     ~MainWindow();
 
+    void scaleImage(double factor);
+    QLabel* label;
+    double scaleFactor;
+    void adjustScrollBar(QScrollBar *scrollBar, double factor);
+    void GenerarMenu();
+    void GenerarConexiones();
 public slots:
     void CargaPlanilla();
+    void ZoomIn();
+    void ZoomOut();
     
 private:
+            QPixmap* pic;
     Ui::MainWindow *ui;
+    //Menu
+        QMenu* Archivo;
+            QMenu* ArchivoNuevo;
+                QList<QAction*> ArchivoNuevoLista;
+            QAction* ArchivoAbrir;
+            QAction* ArchivoImprimir;
+            QAction* ArchivoGuardar;
+            QAction* ArchivoGuardarComo;
+
+        QMenu* Ver;
+            QAction* VerZoomIn;
+            QAction* VerZoomOut;
+            QAction* VerHoja;
+            QAction* VerPagina;
+            QAction* VerEdicion;
+
+        QMenu* Edicion;
+            QAction* EdicionCopiar;
+            QAction* EdicionCortar;
+            QAction* EdicionPegar;
+
+        QMenu* Formato;
+            QMenu* FormatoFuente;
+                QList<QAction*> FormatoFuenteLista;
+            QAction* FormatoBase;
+            QAction* FormatoMinimo;
 };
 
 #endif // MAINWINDOW_H
